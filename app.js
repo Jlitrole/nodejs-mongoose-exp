503e51d0a74a4404708f3fabf8fa5c7e72674610
Joe Laurole <Josue.laurole@gmail.com>
2021-01-14 23:54:32 -0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,6 @@
+var express = require('express');
+var path = reqiure('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require
